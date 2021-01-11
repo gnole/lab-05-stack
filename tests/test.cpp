4c333ee4892847496c2369c6e60c1e56371415b4
gnole <olegbouchin@gmail.com>
2021-01-12 00:37:08 +0300

    test

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,31 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2018 Your Name <your_email>
 
 #include <gtest/gtest.h>
+#include <stack.hpp>
 
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+TEST(PushNPop, test1) {
+  task1::Stack<int> S;
+  S.push(5);
+  EXPECT_EQ(S.head(), 5);
+  S.push(3);
+  EXPECT_EQ(S.head(), 3);
+  S.pop();
+  EXPECT_EQ(S.head(), 5);
+  int a = 10;
+  S.push(a);
+  EXPECT_EQ(S.head(), 10);
+}
+
+TEST(Push_emplace, test2) {
+  task2::Stack<int> S;
+  S.push_emplace(1, 2, 4);
+  EXPECT_EQ(S.head(), 4);
+  S.pop();
+  EXPECT_EQ(S.head(), 2);
+  S.pop();
+  EXPECT_EQ(S.head(), 1);
+  EXPECT_THROW({S.pop();}, std::runtime_error);
+  EXPECT_THROW({S.head();}, std::runtime_error);
+  S.push(10);
+  EXPECT_EQ(S.head(), 10);
 }
